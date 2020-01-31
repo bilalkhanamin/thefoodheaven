d4b8a834b3849fea475825f90ee70cc0986faacc
Bilal Amin khan <pukhtoon004@gmail.com>
2020-01-31 17:03:13 +0500

    Updates

---
+++ precache-manifest.5b46938ec5d4c8af05dbf6c7ab3ad2d6.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "377a87f7d0606204133e6b6fbdd31cb1",
+    "url": "/thefoodheaven/index.html"
+  },
+  {
+    "revision": "17050c224b14be104ba5",
+    "url": "/thefoodheaven/static/css/2.ce67c855.chunk.css"
+  },
+  {
+    "revision": "915545ed478e99748ff1",
+    "url": "/thefoodheaven/static/css/main.52cfca78.chunk.css"
+  },
+  {
+    "revision": "17050c224b14be104ba5",
+    "url": "/thefoodheaven/static/js/2.90705e0f.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/thefoodheaven/static/js/2.90705e0f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "915545ed478e99748ff1",
+    "url": "/thefoodheaven/static/js/main.2fed4e43.chunk.js"
+  },
+  {
+    "revision": "eaa2b7077a5d22b25aaf",
+    "url": "/thefoodheaven/static/js/runtime-main.3e65f374.js"
+  },
+  {
+    "revision": "1136616d4c51aa3294786b846454d662",
+    "url": "/thefoodheaven/static/media/1.1136616d.png"
+  },
+  {
+    "revision": "5987700ad23ff0084fc39facae17eacf",
+    "url": "/thefoodheaven/static/media/1.5987700a.png"
+  },
+  {
+    "revision": "56c430dd4d215398f3c6ecb6fb632803",
+    "url": "/thefoodheaven/static/media/2.56c430dd.png"
+  },
+  {
+    "revision": "d66ea1de91f0d4ad84e57b921bd55c56",
+    "url": "/thefoodheaven/static/media/2.d66ea1de.png"
+  },
+  {
+    "revision": "942ded940bf233808def47cd24e6a568",
+    "url": "/thefoodheaven/static/media/3.942ded94.png"
+  },
+  {
+    "revision": "b24fcd7a15d5c670334a1b7321ec22a1",
+    "url": "/thefoodheaven/static/media/3.b24fcd7a.png"
+  },
+  {
+    "revision": "d2c1c09e614ab1dea9735e8b7746eaad",
+    "url": "/thefoodheaven/static/media/4.d2c1c09e.png"
+  },
+  {
+    "revision": "415a2c0a12225b2bbf5df36d65da3914",
+    "url": "/thefoodheaven/static/media/about.415a2c0a.jpg"
+  },
+  {
+    "revision": "52185247a349f39bf5eb0b0879b017c3",
+    "url": "/thefoodheaven/static/media/facebook.52185247.svg"
+  },
+  {
+    "revision": "139442da7ce6794e0811b135d271fc36",
+    "url": "/thefoodheaven/static/media/hamburger.139442da.svg"
+  },
+  {
+    "revision": "323c7718156816249ea46d775ba6962f",
+    "url": "/thefoodheaven/static/media/instagram.323c7718.svg"
+  },
+  {
+    "revision": "14d0ba08126f0ad5b5a24ae1095c09fb",
+    "url": "/thefoodheaven/static/media/landing.14d0ba08.jpg"
+  },
+  {
+    "revision": "fc8475c210fcabfc1af349e39d176f24",
+    "url": "/thefoodheaven/static/media/logo.fc8475c2.svg"
+  },
+  {
+    "revision": "bfd39ac55b01a18ac6031711b22d74c5",
+    "url": "/thefoodheaven/static/media/page1.bfd39ac5.jpg"
+  },
+  {
+    "revision": "05190203c83c50a12b6be591280a20ab",
+    "url": "/thefoodheaven/static/media/page2.05190203.jpg"
+  },
+  {
+    "revision": "0a3699cd742520d05dd035e1106ca80e",
+    "url": "/thefoodheaven/static/media/page3.0a3699cd.jpg"
+  },
+  {
+    "revision": "87c6558a2ba40bde10232126ed592bda",
+    "url": "/thefoodheaven/static/media/page4.87c6558a.jpg"
+  },
+  {
+    "revision": "7a107399f202c40091e2cdfd766cc905",
+    "url": "/thefoodheaven/static/media/page5.7a107399.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/thefoodheaven/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/thefoodheaven/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/thefoodheaven/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/thefoodheaven/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "b6f75ec74e004f5a70e78ca4674b3764",
+    "url": "/thefoodheaven/static/media/taste_best.b6f75ec7.jpg"
+  },
+  {
+    "revision": "05d8cfef08a267f09a84b7b1fec3fda7",
+    "url": "/thefoodheaven/static/media/twitter.05d8cfef.svg"
+  }
+]);
